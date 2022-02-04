eee9bfe9aa02aafbaf4e87186e66947d7d15df84
Von Souza <94578866+vonsouza@users.noreply.github.com>
2022-02-04 11:29:52 -0300

    Update Main.java
    Updating from Von

--- src/beatty/Main.java
+++ src/beatty/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	    System.out.println("Hello, my name is Daniel.");
 	    System.out.println("Hello Daniel, I'm Adam");
 	    System.out.println("Hello Daniel, I'm Alexander");
+	    System.out.println("Hello Daniel, I'm Von");
     }
 }
